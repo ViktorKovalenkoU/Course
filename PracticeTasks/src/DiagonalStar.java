fce361f24adf332bc8ae6e9cba4734acf421d640
ViktorKovalenkoU <132162098+ViktorKovalenkoU@users.noreply.github.com>
2025-01-31 16:33:36 +0200

    Practice

---
+++ PracticeTasks/src/DiagonalStar.java
@@ -0,0 +1,28 @@
+public class DiagonalStar {
+    public static void main(String[] args) {
+        printSquareStar(5);
+        printSquareStar(8);
+    }
+
+    public static void printSquareStar(int number) {
+        String star = "*";
+        String space = " ";
+        if (number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        for (int row = 1; row <= number; row++) {
+            for (int column = 1; column <= number; column++) {
+                boolean isFirstRowOrColum = row == 1 || column == 1;
+                boolean isLastRowOrColum = row == number || column == number;
+                boolean isDiagonal = row == column || column == (number - row + 1);
+                if (isFirstRowOrColum || isLastRowOrColum || isDiagonal){
+                    System.out.print(star);
+                } else {
+                    System.out.print(space);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
