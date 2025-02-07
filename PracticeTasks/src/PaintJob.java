ccdaa0f981365de41ad0279430449711d1c2c951
ViktorKovalenkoU <132162098+ViktorKovalenkoU@users.noreply.github.com>
2025-02-07 16:55:12 +0200

    Practice

--- PracticeTasks/src/PaintJob.java
+++ PracticeTasks/src/PaintJob.java
@@ -14,7 +14,7 @@ public class PaintJob {
     }
 
     public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
             return -1;
         }
         int bucketsNeeded = 1;
@@ -29,7 +29,7 @@ public class PaintJob {
     }
 
     public static int getBucketCount(double width, double height, double areaPerBucket) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0){
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
             return -1;
         }
         int bucketsNeeded = 1;
@@ -43,7 +43,7 @@ public class PaintJob {
     }
 
     public static int getBucketCount(double area, double areaPerBucket) {
-        if (area <= 0 || areaPerBucket <= 0){
+        if (area <= 0 || areaPerBucket <= 0) {
             return -1;
         }
         int bucketsNeeded = 1;
