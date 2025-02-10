3d71fa6ddbc6f411b33404097aa8256f2bd8f638
ViktorKovalenkoU <132162098+ViktorKovalenkoU@users.noreply.github.com>
2025-02-10 10:58:30 +0200

    Practice

---
+++ PracticeTasks/src/oop/practice7/Floor.java
@@ -0,0 +1,20 @@
+package oop.practice7;
+
+public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        if (width < 0) {
+            width = 0;
+        } else if (length < 0) {
+            length = 0;
+        }
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+}
