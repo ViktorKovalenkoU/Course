c4e50d4cae9d44446d387e4bb4d4cc370fed781b
ViktorKovalenkoU <132162098+ViktorKovalenkoU@users.noreply.github.com>
2025-10-13 14:21:23 +0300

    Practice

--- PracticeTasks/src/leetcode/RangeSumQuery.java
+++ PracticeTasks/src/leetcode/RangeSumQuery.java
@@ -1,27 +1,26 @@
 package leetcode;
 
 public class RangeSumQuery {
-    static int[] nums;
+    static int[] prefixSum;
+
     public static void main(String[] args) {
-        nums = new int[]{-2,0,3,-5,2,-1};
-        NumArray(nums);
-        System.out.println();
-        System.out.println(sumRange(0,2));
-        System.out.println(sumRange(2,5));
-        System.out.println(sumRange(0,5));
+        prefixSum = new int[]{-2, 0, 3, -5, 2, -1};
+        NumArray(prefixSum);
+        System.out.println(sumRange(0, 2));
+        System.out.println(sumRange(2, 5));
+        System.out.println(sumRange(0, 5));
     }
 
     public static void NumArray(int[] nums) {
-        for (int num : nums) {
-            System.out.print(num + " ");
+        int n = nums.length;
+        prefixSum = new int[n + 1];
+        prefixSum[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
         }
     }
 
     public static int sumRange(int left, int right) {
-        int sum = 0;
-        for (int i = left; i <= right; i++) {
-            sum+=nums[i];
-        }
-        return sum;
+        return prefixSum[right + 1] - prefixSum[left];
     }
 }
