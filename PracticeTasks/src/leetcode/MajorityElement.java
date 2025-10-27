4d478d47db0b6e7c85272b32cea99c39f62cce5b
ViktorKovalenkoU <132162098+ViktorKovalenkoU@users.noreply.github.com>
2025-10-27 16:03:15 +0200

    Practice

---
+++ PracticeTasks/src/leetcode/MajorityElement.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums1 = {3, 2, 3};
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+
+        System.out.println(majorityElement(nums1));
+        System.out.println(majorityElement(nums2));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        if (count > nums.length / 2) {
+            return candidate;
+        } else {
+            throw new IllegalArgumentException("No major element exist");
+        }
+    }
+}
