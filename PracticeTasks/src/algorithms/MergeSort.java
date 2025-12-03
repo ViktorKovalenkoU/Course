442ef1bae4a0e59e0e1c26582a21c3ed27d398eb
Viktor <javatrainin1337@gmail.com>
2025-12-03 11:07:22 +0200

    Basic Algorithms

--- PracticeTasks/src/algorithms/MergeSort.java
+++ PracticeTasks/src/algorithms/MergeSort.java
@@ -2,10 +2,18 @@ package algorithms;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int[] numbers = {12,11,13,5,6,7};
+        int[] numbers = {12, 11, 13, 5, 6, 7};
         mergeSort(numbers, 0, numbers.length - 1);
         for (int num : numbers) {
             System.out.print(num + " ");
+        }
+
+        String[] words = {"Banana", "Peanut", "Apple", "Orange", "Coconut"};
+        System.out.println();
+        System.out.println("-----------------");
+        mergerStringSort(words, 0, words.length - 1);
+        for (String word : words) {
+            System.out.print(word + " ");
         }
     }
 
@@ -16,6 +24,16 @@ public class MergeSort {
             mergeSort(arr, left, mid);
             mergeSort(arr, mid + 1, right);
             merge(arr, left, mid, right);
+        }
+    }
+
+    public static void mergerStringSort(String[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergerStringSort(arr, left, mid);
+            mergerStringSort(arr, mid + 1, right);
+            mergeString(arr, left, mid, right);
         }
     }
 
@@ -42,6 +60,32 @@ public class MergeSort {
             }
         }
 
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    public static void mergeString(String[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        String[] L = new String[n1];
+        String[] R = new String[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i].compareTo(R[j]) <= 0) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
         while (i < n1) arr[k++] = L[i++];
         while (j < n2) arr[k++] = R[j++];
     }
