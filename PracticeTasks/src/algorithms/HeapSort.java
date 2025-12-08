e05df43893b2280119d5405972f0035dab8e7931
Viktor <javatrainin1337@gmail.com>
2025-12-08 11:43:12 +0200

    Basic Algorithms

---
+++ PracticeTasks/src/algorithms/HeapSort.java
@@ -0,0 +1,49 @@
+package algorithms;
+
+public class HeapSort {
+    public static void main(String[] args) {
+        String[] words = {"Peanut", "Apple", "Banana", "Orange", "Tamarillo", "Coconut"};
+        heapSort(words);
+        for (String word : words) {
+            System.out.print(word + " ");
+        }
+    }
+
+    public static void heapSort(String[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            String temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(String[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left].compareTo(arr[largest]) > 0) {
+            largest = left;
+        }
+
+        if (right < n && arr[right].compareTo(arr[largest]) > 0) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            String swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+}
